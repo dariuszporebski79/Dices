f1d54a196f1dbe3e31a5e527ab9b21ca74867df8
dariuszporebski79 <dariusz.porebski79@wp.pl>
2023-04-05 22:13:07 +0200

    the second step ;-)

--- dices.py
+++ dices.py
@@ -1,21 +1,37 @@
+# Działa dla 2D6+5 i podobnych:
+# liczba rzutów z przedziału 1-9
+# (ale 1 musi być wpisane przez użytkownika)
+# kostka typu D3, D4, D6 lub D8
+# dodawana lub odejmowana liczba z przedziału 1-9
+# nie obsługuje wyjątków
 from random import randint
 
 
-def dices():
-    number_of_dice_rolls = int(input("Ile razy rzucamy kostką? "))
-    type_of_dice = int(input("Podaj rodzaj kostki spośród następujacych typów:\n"
-                             "3, 4, 6, 8, 10, 12, 20, 100 "))
-    added_number = int(input("Ile dodać lub odjąć od wyniku rzutu/ów kostką? "))
+def dices(roll):
+    roll_list = list(roll)
+    number_of_dice_rolls = int(roll_list[0])
+    type_of_dice = int(roll_list[2])
+    added_number = int(roll_list[4])
+    print(f"Liczba rzutów kostką: {number_of_dice_rolls}\n"
+          f"Typ kostki: {type_of_dice}\n"
+          f"Dodajemy (odejmujemy): {added_number}")
     result = 0
     for i in range(number_of_dice_rolls):
-        result += randint(1, type_of_dice)
-    return result + added_number
+        result_of_roll = randint(1, type_of_dice)
+        print(f"Wylosowano: {result_of_roll}")
+        result += result_of_roll
+    print(f"Wynik rzutu/rzutów kostką: {result}")
+    if roll_list[3] == "+":
+        result += added_number
+    else:
+        result -= added_number
+    return result
 
 
-print(dices())
+your_roll = input("Wskaż rzut kostką: ")
+print(f"Wynik końcowy: {dices(your_roll)}")
 
 
-# roll_of_the_dice
 # xDy+z
 #
 # gdzie:
